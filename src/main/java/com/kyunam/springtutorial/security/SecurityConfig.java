744ab4c4ced268505d6bc8c13a0a39a9724080e3
kyu <xmfpes@naver.com>
2017-08-22 12:42:09 +0900

    Spring Security DataSource DB 로그인 기능 구현

--- src/main/java/com/kyunam/springtutorial/security/SecurityConfig.java
+++ src/main/java/com/kyunam/springtutorial/security/SecurityConfig.java
@@ -19,6 +19,8 @@ import groovy.util.logging.Log;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	DataSource dataSource;
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,12 +33,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-            		.defaultSuccessUrl("/users")
+            		.defaultSuccessUrl("/users/loginForm")
             		.failureUrl("/users/registerForm")
                 .loginPage("/users/loginForm")
+                .loginProcessingUrl("/users/login")
                 .permitAll()
                 .and()
             .logout()
             .permitAll();
     }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		String query1 = "SELECT uid username, upw password, true enabled FROM member where uid = ?";
+		String query2 = "SELECT member uid, role_name role FROM member_role WHERE member = ?";
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(query1)
+		.rolePrefix("ROLE_")
+		.authoritiesByUsernameQuery(query2);
+	}
 }
