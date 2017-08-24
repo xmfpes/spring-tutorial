b5853fd525c7c8883e48b59535a4b61aecec90bb
kyu <xmfpes@naver.com>
2017-08-24 23:43:39 +0900

    체스 컨트롤러 추가

---
+++ src/main/java/com/kyunam/springtutorial/web/ChessController.java
@@ -0,0 +1,14 @@
+package com.kyunam.springtutorial.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chess")
+public class ChessController {
+	@GetMapping("")
+	public String home() {
+		return "/chess/chess";
+	}
+}
